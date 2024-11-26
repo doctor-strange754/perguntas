7ecf1b4aad52c9f157caf0d27bbdddf6d46b8d75
doctor-strange754 <mel.olisilvaa@gmail.com>
2024-11-26 05:01:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'O que é a terceira lei de Newton?',
+    'É a lei da ação e reação, que afirma que, para toda força exercida, existe uma força de mesma intensidade, direção oposta e sentido contrário.'
+)
+
+criaCartao(
+    'Química',
+    'O que são isótopos?',
+    'São átomos do mesmo elemento químico que possuem o mesmo número de prótons, mas diferentes números de nêutrons.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é biodiversidade?',
+    'É a variedade de espécies de seres vivos existentes em uma região ou no planeta.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é uma fração imprópria?',
+    'É uma fração em que o numerador é maior ou igual ao denominador, como 7 sobre 4.'
+)
